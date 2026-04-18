e13dd3cb43877f256bc285159e05e223d0a1641e
Davids0n2 <davidand.tor007@gmail.com>
2026-04-18 15:45:40 -0300

    teste com API

--- Us.js
+++ Us.js
@@ -1,8 +1,43 @@
-const filmes [
-    {
-        imagem:
-        titulo: "Jhon Wick 4", 
-        descricao: "John Wick enfrenta um novo desafio, lutando contra a Alta Cúpula e seus assassinos mais letais.",
+const apikey =  "fe7da511"
+const filmesEN = [ 
+    "interstellar", "Avengers", "Harry Potter", 
+    "Pirates of the Caribbean", "Star Wars", "The Lord of the Rings" , "tron" , "tron legacy","noah"
+]
+
+// Titulos dos filmes em português
+const filmesPT = [
+    "Interestelar", "Vingadores", "Harry Potter", "Piratas do Caribe", "Guerra nas Estrelas" , "O Senhor dos Anéis" , "Tron" , "Tron: O Legado"
+]
+
+
+
+async function CatalagoFilmes() {
+    const filme = document.querySelector(".filmes-container")
+    let html = `<div class = "fcontainer">`
+
+    for (let i = 0; i < filmesEN.length; i++) {
+        const resposta = await fetch(`http://www.omdbapi.com/?t=${filmesEN[i]}&apikey=${apikey}`) //busca  a resposta da API
+        const dados = await resposta.json()
+
+        if(dados.Response === "True"){
+            
+            html +=`
+            <div class = "fcontainer">
+                <div class = "filme">
+                    <img class="poster" src="${dados.Poster}" alt="Poster do filme ${dados.Title}">
+                    <p id="genero">${dados.Genre}</p>
+                    <h2>${filmesPT[i]}</h2>
+                    <p>${dados.Runtime}</p>
+                </div>
+            </div>    
+            `
+            //<p>${dados.Released}</p> mostra a data de lançamento do filme
+            //<p>${dados.imdbRating} / 10</p> mostra a nota do filme
+        }
         
     }
-]
+    html+= `</div>`
+    filme.innerHTML = html
+}
+CatalagoFilmes()
+
